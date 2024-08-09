5ad3c32d8bed946297525af2d37b1a423e74274a
AmrinderSinghKhangura <amrinder.khangura@edu.sait.ca>
2024-08-09 14:29:23 -0400

    added BSTree class that implements BSTREE interface. created blank methods and blank tests for all methods

---
+++ tests/treeImplementation/BSTreeTests.java
@@ -0,0 +1,70 @@
+package treeImplementation;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BSTreeTests {
+
+  @BeforeEach
+  void setUp() {
+  }
+
+  @AfterEach
+  void tearDown() {
+  }
+
+  @Test
+  void getRoot() {
+  }
+
+  @Test
+  void getHeight() {
+  }
+
+  @Test
+  void size() {
+  }
+
+  @Test
+  void isEmpty() {
+  }
+
+  @Test
+  void clear() {
+  }
+
+  @Test
+  void contains() {
+  }
+
+  @Test
+  void search() {
+  }
+
+  @Test
+  void add() {
+  }
+
+  @Test
+  void removeMin() {
+  }
+
+  @Test
+  void removeMax() {
+  }
+
+  @Test
+  void inorderIterator() {
+  }
+
+  @Test
+  void preorderIterator() {
+  }
+
+  @Test
+  void postorderIterator() {
+  }
+}
